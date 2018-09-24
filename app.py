6253a667beebddea7cc5cd2a546e39b2e308ee47
john <john@nader.mx>
2018-09-24 02:00:30 -0500

    no login or sign up at launch, sleep time

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, request
-from flask_login import LoginManager, current_user
+from flask_login import LoginManager, current_user, login_user
 from pony.orm import *
 import models
 from datetime import datetime
 import redis
+# from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+# CSRFProtect(app)
+
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
@@ -50,6 +54,47 @@ def no():
     shipped = models.select(s for s in models.Ship if s.dt_shipped > today_utc)
     no = shipped.filter(lambda n: n.no).count()
     return render_template('no.html', shipped=shipped.count(), no=no, percent=int(no)/int(shipped.count()))
+
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    if request.method == 'POST':
+        data = request.form
+        email = data['email'].lower()
+        password = data['password']
+        user = models.User.get(email=email)
+        if not user:
+            return render_template('login.html', error='User does not exist')
+        if not user.verify_password(password):
+            return render_template('login.html', error='Incorrect Password')
+        login_user(user)
+        return redirect(url_for('index'))
+    if request.method == 'GET':
+        return render_template('login.html')
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == 'GET':
+        return render_template('signup.html')
+    if request.method == 'POST':
+        data = request.form
+        username = data['username'].lower()
+        email = data['email'].lower()
+        password = data['password']
+        if not email or not password:
+            return render_template('signup.html', error="missing email or password")
+        user = models.User.get(email=email)
+        if user:
+            return render_template('signup.html', error="Email already exists")
+        user = models.User(email=email)
+        user.hash_password(password)
+        models.commit()
+        login_user(user)
+        return redirect(url_for('index'))
+
 
 @app.route('/')
 def index():

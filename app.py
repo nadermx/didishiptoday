49d6c84fec0b12139d0162d33e8d2a373dd1f3cc
john <john@nader.mx>
2018-09-23 20:47:04 -0500

    Adding buttons, starting secondary pages

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/yes')
+def yes():
+    return render_template('yes.html')
+
+@app.route('/no')
+def no():
+    return render_template('no.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

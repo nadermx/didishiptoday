76742cd170d3e928950d575b8135c20dbdfc41b1
john <john@nader.mx>
2018-09-23 23:59:11 -0500

    Delete some useless code, add in CSS colors to make it look not so bland

--- app.py
+++ app.py
@@ -37,6 +37,16 @@ def yes():
 
 @app.route('/no', methods=['POST'])
 def no():
+    if current_user.is_authenticated:
+        models.Ship(no=True, user=models.User[current_user.id], dt_shipped=datetime.utcnow)
+    else:
+        anon_ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+        if not r.get(anon_ip):
+            end_of_day = datetime.utcnow().replace(hour=23, minute=59, second=59, microsecond=999)
+            r.setex(anon_ip, 'visited_today', end_of_day)
+            models.Ship(no=True, dt_shipped=datetime.utcnow())
+    today_utc = datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)
+    shipped = models.select(s for s in models.Ship if s.dt_shipped > today_utc)
     return render_template('no.html')
 
 @app.route('/')
